90acc597b5ca8f4bb6d23957c95aa4323ca50464
stillysyw <154344530+stillysyw@users.noreply.github.com>
2024-12-23 14:30:32 +0400

    Delete save_text_to_pdf_file.py

--- save_text_to_pdf_file.py
+++
@@ -1,9 +0,0 @@
-from fpdf import FPDF
-
-file = open("/data/summary.txt","r")
-pdf = FPDF()
-pdf.add_page()
-for text in file:
-    pdf.set_font("Times","BI", size=15)
-    pdf.multi_cell(w=0,h=10, txt=text, align="J")
-    pdf.output("/data/result_summary_in_pdf_format.pdf")
