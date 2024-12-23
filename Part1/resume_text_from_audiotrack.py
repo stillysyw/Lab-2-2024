af2c64c8844a24a8abd142da177568cc6cbb4738
stillysyw <154344530+stillysyw@users.noreply.github.com>
2024-12-23 14:31:52 +0400

    Update resume_text_from_audiotrack.py

--- Part1/resume_text_from_audiotrack.py
+++ Part1/resume_text_from_audiotrack.py
@@ -1,16 +1,3 @@
-# import requests
-# API_URL = "https://api-inference.huggingface.co/models/slauw87/bart_summarisation"
-# API_TOKEN = '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'
-# headers = {"Authorization": f"Bearer {API_TOKEN}"}
-
-# with open('/data/text.txt', "rb") as f:
-#     data = f.read()
-#     response = requests.post(API_URL, headers=headers, json={'inputs': f"{data}",})
-#     result = response.json()
-#     summary_text = result[0]['summary_text']
-#     text_file = open("/data/summary.txt", "w+")
-#     text_file.write(summary_text)
-#     text_file.close() 
 import requests
 import json
 
