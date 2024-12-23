0712707274b18437cabd8742670479d16bdb6600
stillysyw <154344530+stillysyw@users.noreply.github.com>
2024-12-23 14:32:04 +0400

    Update transform_audiotrack_to_text.py

--- Part1/transform_audiotrack_to_text.py
+++ Part1/transform_audiotrack_to_text.py
@@ -1,16 +1,3 @@
-# import requests
-# API_URL = "https://api-inference.huggingface.co/models/openai/whisper-small"
-# API_TOKEN = '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'
-# headers = {"Authorization": f"Bearer {API_TOKEN}"}
-
-# with open('/data/audio.aac', "rb") as f:
-#     data = f.read()
-#     response = requests.post(API_URL, headers=headers, data=data)
-#     result = response.json()
-#     text_from_audio = result['text']
-#     text_file = open("/data/text.txt", "w+")
-#     text_file.write(text_from_audio)
-#     text_file.close()
 import requests
 import json
 
